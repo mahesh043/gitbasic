3b3d66a4003a89eb97d7378b53b3200b8b56a662
mahesh043 <74089277+mahesh043@users.noreply.github.com>
2020-11-06 22:28:53 -0800

    Add files via upload

---
+++ python practice/Day2(15-09-2020)/sample2.py
@@ -0,0 +1,9 @@
+num1=20
+num2=30
+print("before swapping:",num1,num2)
+
+temp=num2
+num2=num1
+num1=temp
+print("after swapping:",num1,num2)
+
---
+++ python practice/Day2(15-09-2020)/sample3.py
@@ -0,0 +1,6 @@
+#val=50
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+print(a,type(a))
+print(a+b)
+
